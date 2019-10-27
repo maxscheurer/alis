70d4c95a58e506557176acfce1a3c4787ef89fa8
Maximilian Scheurer <max.scheurer@me.com>
2019-10-27 15:57:43 +0100

    some tweaks

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@ set -e
 
 # Arch Linux Install Script (alis) installs unattended, automated
 # and customized Arch Linux system.
-# Copyright (C) 2018 picodotdev
+# Copyright (C) 2018 picotdotdev
 
 rm -f alis.conf
 rm -f alis.sh
@@ -15,15 +15,15 @@ rm -f alis-recovery.sh
 rm -f alis-recovery-asciinema.sh
 rm -f alis-recovery-reboot.sh
 
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis.conf
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis.sh
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis-asciinema.sh
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis-reboot.sh
+wget https://raw.githubusercontent.com/maxscheurer/alis/master/alis.conf
+wget https://raw.githubusercontent.com/maxscheurer/alis/master/alis.sh
+wget https://raw.githubusercontent.com/maxscheurer/alis/master/alis-asciinema.sh
+wget https://raw.githubusercontent.com/maxscheurer/alis/master/alis-reboot.sh
 
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis-recovery.conf
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis-recovery.sh
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis-recovery-asciinema.sh
-wget https://raw.githubusercontent.com/picodotdev/alis/master/alis-recovery-reboot.sh
+wget https://raw.githubusercontent.com/maxscheurer/alis/master/alis-recovery.conf
+wget https://raw.githubusercontent.com/maxscheurer/alis/master/alis-recovery.sh
+wget https://raw.githubusercontent.com/maxscheurer/alis/master/alis-recovery-asciinema.sh
+wget https://raw.githubusercontent.com/maxscheurer/alis/master/alis-recovery-reboot.sh
 
 chmod +x alis.sh
 chmod +x alis-asciinema.sh
